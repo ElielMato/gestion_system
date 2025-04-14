73256bc33ddf380bba401cb5a6e7392c45f1d8e7
Eliel Mato <elielmato360@gmail.com>
2025-04-14 09:00:58 -0300

    #1 Create of the Basic Structure

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 import os
 from app.config import config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app() -> Flask:
     app_context = os.getenv('FLASK_CONTEXT')
@@ -12,6 +14,7 @@ def create_app() -> Flask:
     app.config.from_object(f)
 
     db.init_app(app)
+    migrate.init_app(app, db)
     
     @app.shell_context_processor    
     def ctx():
